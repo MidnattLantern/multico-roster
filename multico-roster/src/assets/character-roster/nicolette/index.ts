dfe5de0a563e39442cc4a4b638d78a0d01375251
Alma <isakvent@outlook.com>
2025-01-29 18:00:48 +0100

    create Nicolette pages component

---
+++ multico-roster/src/assets/character-roster/nicolette/index.ts
@@ -0,0 +1,3 @@
+export { default as NicoletteImage1 } from "./nicolette-1.png";
+export { default as NicoletteImage2 } from "./nicolette-2.png";
+export { default as NicoletteImage3 } from "./nicolette-3.png";
